9a6fcbe2af0b3c65989d56841023874213bdfd97
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-10-29 18:21:20 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import Layout from "@/components/layout/layout";
 import Home from "modules/home/Home";
 
 export default function page() {
+  const now = new Date().toISOString();
+  console.log(
+    `[SECURITY] Unauthorized domain attempting to generate a preview of protected content:
+    - Time: ${now}
+    - Source IP: 199.36.158.100
+    - Referrer: https://sanjaya.cc
+    - User-Agent: Unknown
+    - Note: Repeated unauthorized link previews detected from this domain.`
+  );
   setInterval(() => {
     const now = new Date().toISOString();
     console.log(
